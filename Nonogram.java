3ed598210200d05c8a53de84cc6dceb9337db58d
Adam <ADAM594@adams-mbp.lan>
2018-01-09 20:30:19 -0800

    Done with print_board. Works with 5x5 board. Wrote a simple unit test to print it for now.

--- Nonogram.java
+++ Nonogram.java
@@ -1,8 +1,35 @@
-
-
 public class Nonogram {
-
-  // Parfait
+    
+    public static void main(String[] argv) {
+        //run_game();
+        UnitTest();
+    }
+    
+    public static char[][] board = {
+        {'_', '_', '#', '_', '#'},
+        {'#', '#', '#', '#', '_'},
+        {'_', '#', '#', '#', '#'},
+        {'#', '_', '_', '_', '#'},
+        {'_', '_', '_', '_', '#'}
+    };
+ 
+    static int bNums[][] = {
+        // Col Nums
+        {1, 1, -1, -1, -1},
+        {2, -1, -1, -1, -1},
+        {3, -1, -1, -1, -1},
+        {2, -1, -1, -1, -1},
+        {1, 3, -1, -1, -1},
+      
+        // Row Nums
+         {1, 1, -1, -1, -1},
+        {4, -1, -1, -1, -1},
+        {4, -1, -1, -1, -1},
+        {1, 1, -1, -1, -1},
+        {1, -1, -1, -1, -1},
+    };
+    
+ // Parfeit
   static char[][] get_board() {
 
   }
@@ -12,32 +39,84 @@ public class Nonogram {
 
   }
 
-  // Adam
-  static void print_board(char[][] board, int[][] numbers) {
-
-  }
-
+   
   // John
   static void run_game() {
 
   }
 
-  public static void main(String[] argv) {
-    run_game()
+  
+  // Adam
+  static void print_board(char[][] board, int[][] numbers) {
+      // Leave board a 5x5
+      int rLen = (int)board[0].length;
+      char rowCords[] = new char[] {'A', 'B', 'C', 'D', 'E'};
+      
+      // Print col nums
+      for(int i = 0; i < (rLen/2) ; i++)
+      {
+          System.out.print("\n       ");
+          int j = 0;
+          while(j < rLen)
+          {
+              if(numbers[j][i] == -1)
+              {
+                  System.out.print("  ");
+              }
+              
+              else {
+                  System.out.print(numbers[j][i] + " ");
+              }
+              
+              j++;
+          }
+      }
+      System.out.print("\n");
+      
+      
+      // Print Row nums and board
+      for(int i = 0; i < board[0].length; i++)
+      {
+          String row = "";
+          for(int a = 0; a < rLen; a++)
+          {
+              if(numbers[i][a] == -1)
+              {
+                  a = rLen;
+              }
+              
+              else {
+                  row += (Integer.toString(numbers[i][a]) + " ");
+              }
+          }
+          
+          System.out.printf("%6s ", row);
+          
+          for(int j = 0; j < board[i].length; j++)
+          {
+              System.out.print(board[i][j] + " ");
+          }
+          
+          System.out.print(rowCords[i] + "\n");
+          
+      }
+      
+      // Print Col Cords
+      System.out.print("       ");
+      for(int i = 1; i < 6; i++)
+      {
+          System.out.print(i + " ");
+      }
+      System.out.print("\n");
   }
-
-  // public static char[][] board = {
-  //   {'_', '_', '#', '_', '#'},
-  //   {'#', '#', '#', '#', '_'},
-  //   {'_', '#', '#', '#', '#'},
-  //   {'#', '_', '_', '_', '#'},
-  //   {'_', '_', '_', '_', '#'}
-  // };
-
-  // public static void print_board(char[][] b) {
-  //   for (char[] bl : b) {
-  //     System.out.printf("%s %s %s %s %s\n",bl[0],bl[1],bl[2],bl[3],bl[4]);
-  //   }
-  // }
+  
+  static void UnitTest()
+  {
+      // Print Board Unit Test
+      System.out.print("\n~Unit Test 0: Print Board~");
+      print_board(board, bNums);
+      
+      // Add your units tests below:
+  }
 
 }
