eada8e2733e3221b402a01c87c769b9f5dad3408
John FitzGerald <iconium9000@gmail.com>
2018-01-09 17:01:44 -0800

    v0.0.2
    break things down

--- Nonogram.java
+++ Nonogram.java
@@ -2,22 +2,42 @@
 
 public class Nonogram {
 
-  public static char[][] board = {
-    {'_', '_', '#', '_', '#'},
-    {'#', '#', '#', '#', '_'},
-    {'_', '#', '#', '#', '#'},
-    {'#', '_', '_', '_', '#'},
-    {'_', '_', '_', '_', '#'}
-  };
+  // Parfeit
+  static char[][] get_board() {
 
-  public static void print_board(char[][] b) {
-    for (char[] bl : b) {
-      System.out.printf("%s %s %s %s %s\n",bl[0],bl[1],bl[2],bl[3],bl[4]);
-    }
+  }
+
+  // Zac
+  static int[][] get_numbers(char[][] board) {
+
+  }
+
+  // Adam
+  static void print_board(char[][] board, int[][] numbers) {
+
+  }
+
+  // John
+  static void run_game() {
+
   }
 
   public static void main(String[] argv) {
-    print_board(board);
+    run_game()
   }
+
+  // public static char[][] board = {
+  //   {'_', '_', '#', '_', '#'},
+  //   {'#', '#', '#', '#', '_'},
+  //   {'_', '#', '#', '#', '#'},
+  //   {'#', '_', '_', '_', '#'},
+  //   {'_', '_', '_', '_', '#'}
+  // };
+
+  // public static void print_board(char[][] b) {
+  //   for (char[] bl : b) {
+  //     System.out.printf("%s %s %s %s %s\n",bl[0],bl[1],bl[2],bl[3],bl[4]);
+  //   }
+  // }
 
 }
