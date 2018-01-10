263246af897b265d8502470594f10f30b8c1d20d
John FitzGerald <iconium9000@gmail.com>
2018-01-09 16:40:40 -0800

    v0.0.1
    First compile

--- Nonogram.java
+++ Nonogram.java
@@ -2,7 +2,7 @@
 
 public class Nonogram {
 
-  public static char[][] b = {
+  public static char[][] board = {
     {'_', '_', '#', '_', '#'},
     {'#', '#', '#', '#', '_'},
     {'_', '#', '#', '#', '#'},
@@ -12,12 +12,12 @@ public class Nonogram {
 
   public static void print_board(char[][] b) {
     for (char[] bl : b) {
-      System.out.printf("%s %s %s %s %s",bl[0],bl[1],bl[2],bl[3],bl[4])
+      System.out.printf("%s %s %s %s %s\n",bl[0],bl[1],bl[2],bl[3],bl[4]);
     }
   }
 
   public static void main(String[] argv) {
-
+    print_board(board);
   }
 
 }
